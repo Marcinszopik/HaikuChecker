3dac0e30912230a180f3d28790a15125d8aa7fce
David Tumpowsky and Justin Roller <justin@>
2018-05-23 16:23:09 -0700

    refactor files to be object oriented, program determines whether a displayed haiku contains the appropriate amount of syllables

--- src/haiku.js
+++ src/haiku.js
@@ -1,1 +1,109 @@
+export class Haiku {
 
+  constructor (textHaiku,firstLine, secondLine, thirdLine)
+  {
+    this.textHaiku=textHaiku;
+    this.firstLine=firstLine;
+    this.secondLine=secondLine;
+    this.thirdLine=thirdLine;
+  }
+
+  LocateNumberOfSyllables() {
+      var characterArr = this.textHaiku.split('');
+      const vowels = ["a","e","i","o","u"];
+      let numberOfVowels=0;
+        // counts vowels
+      for (var i =0; i < characterArr.length; i++) {
+        for (var j =0; j < vowels.length; j++) {
+          //subtract vowel count if two vowels are next to eachother
+          if (characterArr[i] == vowels[j] && characterArr[i+1] == vowels[j]) {
+            numberOfVowels += 0;
+          }
+          //counts vowels
+          else if (characterArr[i] == vowels[j]) {
+            numberOfVowels += 1;
+          }
+        }
+        //subtract vowel count if e is silent
+        if ((characterArr[i] == " ") && (characterArr[i-1] == "e")) {
+          numberOfVowels += 0;
+        }
+      }
+      return numberOfVowels;
+    }
+
+
+    LocateFirstLineSyllables() {
+        // counts vowels
+      let firstLineArr = this.firstLine.split('');
+      const vowels = ["a","e","i","o","u"];
+      let numberOfVowels=0;
+
+      for (var i =0; i < firstLineArr.length; i++) {
+        for (var j =0; j < vowels.length; j++) {
+          //subtract vowel count if two vowels are next to eachother
+          if (firstLineArr[i] == vowels[j] && firstLineArr[i+1] == vowels[j]) {
+            numberOfVowels += 0;
+          }
+          //counts vowels
+          else if (firstLineArr[i] == vowels[j]) {
+            numberOfVowels += 1;
+          }
+        }
+        //subtract vowel count if e is silent
+        if ((firstLineArr[i] == " ") && (firstLineArr[i-1] == "e")) {
+          numberOfVowels += 0;
+        }
+      }
+      return numberOfVowels;
+    }
+
+
+    LocateSecondLineSyllables() {
+      let secondLineArr = this.secondLine.split('');
+      const vowels = ["a","e","i","o","u"];
+      let numberOfVowels=0;
+        // counts vowels
+      for (var i =0; i < secondLineArr.length; i++) {
+        for (var j =0; j < vowels.length; j++) {
+          //subtract vowel count if two vowels are next to eachother
+          if (secondLineArr[i] == vowels[j] && secondLineArr[i+1] == vowels[j]) {
+            numberOfVowels += 0;
+          }
+          //counts vowels
+          else if (secondLineArr[i] == vowels[j]) {
+            numberOfVowels += 1;
+          }
+        }
+        //subtract vowel count if e is silent
+        if ((secondLineArr[i] == " ") && (secondLineArr[i-1] == "e")) {
+          numberOfVowels += 0;
+        }
+      }
+      return numberOfVowels;
+    }
+
+    LocateThirdLineSyllables() {
+      // counts vowels
+      let thirdLineArr = this.thirdLine.split('');
+      const vowels = ["a","e","i","o","u"];
+      let numberOfVowels=0;
+      for (var i =0; i < thirdLineArr.length; i++) {
+        for (var j =0; j < vowels.length; j++) {
+          //subtract vowel count if two vowels are next to eachother
+          if (thirdLineArr[i] == vowels[j] && thirdLineArr[i+1] == vowels[j]) {
+            numberOfVowels += 0;
+          }
+          //counts vowels
+          else if (thirdLineArr[i] == vowels[j]) {
+            numberOfVowels += 1;
+          }
+        }
+        //subtract vowel count if e is silent
+        if ((thirdLineArr[i] == " ") && (thirdLineArr[i-1] == "e")) {
+          numberOfVowels += 0;
+        }
+      }
+      return numberOfVowels;
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,41 +1,40 @@
+//
+// import { LocateFirstLineSyllables } from './haiku.js';
+// import { LocateSecondLineSyllables } from './haiku.js';
+// import { LocateThirdLineSyllables }  from './haiku.js';
+import { Haiku } from './haiku.js';
 
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-var characterArr = [];
-var vowels = ["a","e","i","o","u"]
-var numberOfVowels = 0;
-
 $(document).ready(function() {
     var textHaiku = $('.haiku-text').text();
+    var firstLine = $("#line-one").text();
+    var secondLine = $("#seven-syllables").text();
+    var thirdLine = $("#line-three").text();
     console.log(textHaiku);
-    characterArr = textHaiku.split('');
 
-    function LocateNumberOfSyllables() {
-        // counts vowels
-      for (var i =0; i < characterArr.length; i++) {
-        for (var j =0; j < vowels.length; j++) {
-          //subtract vowel count if two vowels are next to eachother
-          if ((characterArr[i] == vowels[j]) && (characterArr[i+1] == vowels[j])) {
-            numberOfVowels -= 1;
-          }
-          //counts vowels
-          else if (characterArr[i] == vowels[j]) {
-            numberOfVowels += 1;
-          }
-        }
-        //subtract vowel count if e is silent
-        if ((characterArr[i] == " ") && (characterArr[i-1] == "e")) {
-          numberOfVowels -= 1;
-        }
-      }
+    var myHaiku=new Haiku(textHaiku, firstLine, secondLine, thirdLine);
+
+
+    let numberOfVowels=0;
+    numberOfVowels+=myHaiku.LocateFirstLineSyllables();
+    numberOfVowels+=myHaiku.LocateSecondLineSyllables();
+    numberOfVowels+=myHaiku.LocateThirdLineSyllables();
+
+
+  $("button#verify-haiku").click(function(event) {
+    if (numberOfVowels > 17) {
+      $("#result").text("NOT AN HAIKU!");
+    } else {
+      $("#result").text("DEFINITELY A HAIKU!");
     }
 
-  LocateNumberOfSyllables()
-
-
-    console.log(characterArr);
     console.log(numberOfVowels);
+  });
+
+
+
 });
